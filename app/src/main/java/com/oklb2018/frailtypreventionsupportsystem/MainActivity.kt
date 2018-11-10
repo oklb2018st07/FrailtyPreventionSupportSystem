1f695f5e21e7c053aeb75a5e5f008c580640341e
Shumpei Shimizu <oklb.2018.st07@gmail.com>
2018-11-10 16:48:58 +0900

    test commit for develop branch

--- app/src/main/java/com/oklb2018/frailtypreventionsupportsystem/MainActivity.kt
+++ app/src/main/java/com/oklb2018/frailtypreventionsupportsystem/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var x: String = "hello world"
+        var y: Int = 2
     }
 
 }
