ce765e13df83fd82655e743d0e4f04c45027fdc9
Shumpei Shimizu <oklb.2018.st07@gmail.com>
2018-11-12 21:07:26 +0900

    operation check test

--- app/src/main/java/com/oklb2018/frailtypreventionsupportsystem/MainActivity.kt
+++ app/src/main/java/com/oklb2018/frailtypreventionsupportsystem/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.oklb2018.frailtypreventionsupportsystem
 
+import android.graphics.Point
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var x: String = "hello world"
-        var y: Int = 2
+        text_view1.setText(x)
+
+        var p: Point = Point()
+        this.windowManager.defaultDisplay.getRealSize(p)
+        Log.v("DisplaySize", "x = ${p.x}, y = ${p.y}")
+        text_view1.text = "x = ${p.x}, y = ${p.y}"
+
+        val texts = arrayOf("my", "name", " is", "shum", "desu")
+        val adapter = ArrayAdapter(this, R.layout.list_items_2, texts)
+        listView1.setAdapter(adapter)
+
+        listView1.setOnItemClickListener { adapterView, view, position, id -> Log.v("tags", "onClick!") }
     }
 
 }
