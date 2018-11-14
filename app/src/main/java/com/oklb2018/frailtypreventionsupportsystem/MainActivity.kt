6e6a927d9d5aa0eb80f55431aa279b308d823f62
Shumpei Shimizu <oklb.2018.st07@gmail.com>
2018-11-14 20:45:47 +0900

    Making the base activity

--- app/src/main/java/com/oklb2018/frailtypreventionsupportsystem/MainActivity.kt
+++ app/src/main/java/com/oklb2018/frailtypreventionsupportsystem/MainActivity.kt
@@ -1,30 +1,86 @@
 package com.oklb2018.frailtypreventionsupportsystem
 
-import android.graphics.Point
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val titles = listOf("Check List", "Walking", "Meals", "Brain Training", "My Data", "Find Activity")
+
+    val images = listOf(
+        R.drawable.icon01, R.drawable.icon02, R.drawable.icon03,
+        R.drawable.icon04, R.drawable.icon05, R.drawable.icon06
+    )
+
+    val menus = List(titles.size) { i -> MenuListData(titles[i], images[i]) }
+
+    /**
+     * 周知の通り，onCreate
+     * このメソッドから処理が開始される（定義厨におこられそうな説明）
+     */
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var x: String = "hello world"
-        text_view1.setText(x)
 
-        var p: Point = Point()
-        this.windowManager.defaultDisplay.getRealSize(p)
-        Log.v("DisplaySize", "x = ${p.x}, y = ${p.y}")
-        text_view1.text = "x = ${p.x}, y = ${p.y}"
+        val menuListAdapter = MenuListAdapter(this, menus)
+        listView1.adapter = menuListAdapter
 
-        val texts = arrayOf("my", "name", " is", "shum", "desu")
-        val adapter = ArrayAdapter(this, R.layout.list_items_2, texts)
-        listView1.setAdapter(adapter)
+        listView1.setOnItemClickListener { parent, view, pos, id ->
+            run {
+                Log.d("MainActivity", "button click")
+                Log.d("MainActivity", "second click")
+                for (v in parent.touchables) v.setBackgroundResource(R.drawable.colored_rectangler_00)
+                parent.getChildAt(pos).setBackgroundResource(R.drawable.colored_rectangler_01)
+            }
+        }
+    }
+}
 
-        listView1.setOnItemClickListener { adapterView, view, position, id -> Log.v("tags", "onClick!") }
+/**
+ * MenuListAdapterの生成に対して必要なデータを保持するdata class
+ */
+data class MenuListData(val title: String, val imageId: Int)
+
+/**
+ * MenuListAdapterにおける，リソース保持用のdata class
+ */
+data class ViewHolder(val titleTextView: TextView, val imageView: ImageView)
+
+/**
+ * メニューとして表示されるCustom ListView用のアダプタ．
+ * data class MenuListDataのListを受け取り，viewを生成する
+ */
+class MenuListAdapter(context: Context, menus: List<MenuListData>) : ArrayAdapter<MenuListData>(context, 0, menus) {
+    private val layoutInflater: LayoutInflater =
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var viewHolder: ViewHolder? = null
+        var view = convertView
+        if (view == null) {
+            view = layoutInflater.inflate(R.layout.list_items_1, parent, false)
+            viewHolder = ViewHolder(view.findViewById(R.id.titleTextView), view.findViewById(R.id.imageView))
+            view.tag = viewHolder
+        } else {
+            viewHolder = view.tag as ViewHolder
+        }
+
+        val listItem = getItem(position)
+        viewHolder.titleTextView.text = listItem!!.title
+        viewHolder.imageView.setImageBitmap(BitmapFactory.decodeResource(context.resources, listItem.imageId))
+
+        return view!!
     }
 
 }
+
